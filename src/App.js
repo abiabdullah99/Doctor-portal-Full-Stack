4bfc2ef3f4220332cfad74d6c3804bd2d0b7b3ac
Abdullah <mdabdullahnoman777@gmail.com>
2022-05-10 18:34:44 +0600

    Footer Add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home/Home';
+import Footer from './Pages/Shared/Navbar/Footer/Footer';
 import Navbar from './Pages/Shared/Navbar/Navbar';
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
      <Routes>
        <Route path='/' element={<Home></Home>}></Route>
      </Routes>
+     <Footer></Footer>
    </div>
   );
 };
